9cad8bb31b9957ab78375c6db0cf35d63502487f
jespitiar33 <jespitiar33@gmail.com>
2024-12-17 23:05:49 -0600

    update no nbformat

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-import nbformat
 
 car_data = pd.read_csv(r'C:\Users\admin\Documents\Docs JJER\Sprint 7 Final\vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón

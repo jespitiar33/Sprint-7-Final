a654f87db9ec37b3dacb7d5aa348b64da92289d2
jespitiar33 <jespitiar33@gmail.com>
2024-12-17 23:04:13 -0600

    update nbformat

--- vehicles_env.py
+++ vehicles_env.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-import nbformat
 
 df = pd.read_csv(r'C:\Users\admin\Documents\Docs JJER\Sprint 7 Final\vehicles_us.csv')
 
 print(df)
+
+!pip install nbformat

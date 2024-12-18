7d5e910a32ff2e9d27714310c6113c73d9069353
jespitiar33 <jespitiar33@gmail.com>
2024-12-18 00:46:05 -0600

    update path

--- vehicles_env.py
+++ vehicles_env.py
@@ -3,6 +3,6 @@ import streamlit as st
 import plotly.express as px
 import nbformat
 
-car_data = pd.read_csv(r'C:\Users\admin\Documents\Docs JJER\Sprint 7 Final\vehicles_us.csv')
+car_data = pd.read_csv('vehicles_us.csv')
 
 print(car_data)
